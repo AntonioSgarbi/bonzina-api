6476cfb0725214e24f5a53eb8d259ec1b0c650b9
AntonioSgarbi <antonio.sgarbi@hotmail.com>
2021-07-13 02:16:16 -0300

    initial setup

---
+++ src/main/java/com/antoniosgarbi/BonzinaApiApplication.java
@@ -0,0 +1,13 @@
+package com.antoniosgarbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonzinaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonzinaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antoniosgarbi/BonzinaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antoniosgarbi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonzinaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
