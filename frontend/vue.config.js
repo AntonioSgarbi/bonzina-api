36fcdd3b8ecdf860cb7a191ce738c5d453fe9c65
AntonioSgarbi <antonio.sgarbi@hotmail.com>
2021-09-08 01:45:22 -0300

    change title on deploy

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,13 @@ module.exports = {
   lintOnSave: false
 }
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/bonzina-api/'
-      : '/'
+  
+  publicPath: process.env.NODE_ENV === 'production' ? '/bonzina-api/' : '/bonzina-api/',
+
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'Clínica Médica';
+      return args;
+    });
+  }
 }
